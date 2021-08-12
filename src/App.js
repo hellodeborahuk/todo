1f2591a82c19f4787f8b03ed0ebb4650f367374c
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-08-12 13:25:47 +0100

    Created todo form component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-bold">To Do App</h1>
+      <TodoForm /> 
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handleChange = e => {
+      setInput(e.target.value);
+  }
+
+  const handleSubmit = e => {
+      e.preventDefault();
+    
+      props.onSubmit({
+          id: Math.floor(Math.random() * 10000),
+          text: {input},
+      });
+
+      setInput(''); // resets to empty string
+  }
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit} className="space-x-6">
+      <input
+        type="text"
+        placeholder="Add a todo..."
+        value={input}
+        name="text"
+        className="border border-blue-500 p-2"
+        onChange={handleChange}
+      />
+      <button className="bg-blue-500 text-blue-50 rounded-lg px-4 py-2 hover:bg-blue-600">Add todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
